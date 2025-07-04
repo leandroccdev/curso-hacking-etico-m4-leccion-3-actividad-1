ff3a0754387915e009f7d87f23dd350119b77a8b
Leandro Cañellas <federico.leandro.c@gmail.com>
2025-07-03 20:59:25 -0400

    Elimina ruta index.js.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -13,7 +12,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
