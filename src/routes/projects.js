7df77469b2ab73f84b8f26aea6c17eee8128fa1c
Leandro Cañellas <federico.leandro.c@gmail.com>
2025-07-04 18:27:14 -0400

    Agrega funcionalidad para editar proyectos.

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -115,26 +115,50 @@ router.post(
  * 
  * Objeto esperado:
  * {
- *      "id": integer,
- *      "title": string,
- *      "description": string,
- *      status: integer
+ *      "id": integer, // campo requerido
+ *      "title": string, // opcional
+ *      "description": string, // opcional
+ *      status: integer // opcional
  * }
  */
 router.put(
     '/',
+    detect_authorization_xss,
+    detect_body_xss,
     auth_verify,
     required_role([
         user_roles.administrator,
         user_roles.editor
     ]),
+    sanitize_body,
     async (req, res, next) => {
-        let {id, title, description, status}
-        
-        // Obtiene el token decodificado
-        const decoded_token = get_decoded_token(req.headers?.authorization);
+        try {
+            const {id, title, description, status} = req.body;
 
-        
+            if (!id)
+                return next(create_error(400, "'id' field is required."));
+
+            // Obtiene el token decodificado
+            const decoded_token = get_decoded_token(req.headers?.authorization);
+
+            // Obtiene el proyecto para realizar la edición
+            const project = await db.Project.findByPk(id);
+
+            // No se encontró el proyecto
+            if (!project)
+                return next(create_error(404, 'Resource not found.'));
+
+            // Actualiza todos los campos con cambios menos el id
+            await project.update({
+                title: title,
+                description: description,
+                status: status
+            });
+
+            res.status(200).json();
+        } catch (err) {
+            next(create_error(err.status, err.message));
+        }
     }
 );
 
