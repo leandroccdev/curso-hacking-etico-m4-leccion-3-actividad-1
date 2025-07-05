4c5ecee3edc58db9291fe76b786102b70a037c24
Leandro Cañellas <federico.leandro.c@gmail.com>
2025-07-04 21:36:47 -0400

    Setea identación de appjs. a 4 espacios.

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const logger = require('morgan');
 
 const usersRouter = require('./routes/users');
 const projectsRouter = require('./routes/projects');
+const tasksRouter = require('./routes/tasks');
 
 var app = express();
 
@@ -14,24 +15,25 @@ app.use(cookieParser());
 
 app.use('/user', usersRouter);
 app.use('/project', projectsRouter);
+app.use('/tasks', tasksRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(create_error(404));
+app.use(function (req, res, next) {
+    next(create_error(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500).json({
-    error: {
-      message: req.app.get('env') === 'development' ? err.message : "The request couldn't be processed."
-    }
-  });
+    // render the error page
+    res.status(err.status || 500).json({
+        error: {
+            message: req.app.get('env') === 'development' ? err.message : "The request couldn't be processed."
+        }
+    });
 });
 
 module.exports = app;
